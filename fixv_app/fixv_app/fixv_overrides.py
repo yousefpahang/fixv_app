b3c1507327c4799e1c4a51069ba765697794ff02
youseff <y.pahang@gmail.com>
2024-01-30 23:25:50 +0400

    testing override

---
+++ fixv_app/fixv_app/fixv_overrides.py
@@ -0,0 +1,7 @@
+import frappe
+from frappe.model.documeent import Document
+
+class myBank(Document):
+    def validate(self):
+        if len(self.swift_number) <11:
+            frappe.msgprint("Swift number should be less than 11")
