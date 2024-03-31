6f10affc8b09ada724225455403c499ed9dcf7e8
yousefpahang <y.pahang@gmail.com>
2024-03-31 10:49:34 +0000

    add project link in quotation

--- fixv_app/hooks.py
+++ fixv_app/hooks.py
@@ -12,7 +12,7 @@ app_license = "mit"
 # include js, css files in header of desk.html
 # app_include_css = "/assets/fixv_app/css/fixv_app.css"
 # app_include_js = "/assets/fixv_app/js/fixv_app.js"
-
+app_include_js = "/assets/fixv_app/js/project_dashboard_extension.js"
 # include js, css files in header of web template
 # web_include_css = "/assets/fixv_app/css/fixv_app.css"
 # web_include_js = "/assets/fixv_app/js/fixv_app.js"
